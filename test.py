69fcb96a04f3a5182fb820ff075d799d987513ee
André Costa <andreccosta@me.com>
2018-02-26 20:48:41 +0000

    Up refresh rate

--- test.py
+++ test.py
@@ -6,24 +6,19 @@ import multigraph
 blinkt.set_clear_on_exit()
 blinkt.set_brightness(0.1)
 
-colors = [[0, 0, 0]] * blinkt.NUM_PIXELS
-
 cpu_color = (0, 0, 200)
 mem_color = (200, 0, 0)
 
 ceiling = 100
-floor = 5
-step = 5
-v = 5
+floor = step = v = 1
 
 while True:
-  v += step
-  if v == ceiling or v == floor:
-    step = -step
+    v += step
+    if v == ceiling or v == floor:
+        step = -step
 
-  cpu_perc = v / 100.0
-  mem_perc = v / 100.0
+    cpu_perc = mem_perc = v / 100.0
 
-  multigraph.show(cpu_perc, mem_perc, cpu_color, mem_color)
+    multigraph.show(cpu_perc, mem_perc, cpu_color, mem_color)
 
-  time.sleep(0.1)
+    time.sleep(0.01)
